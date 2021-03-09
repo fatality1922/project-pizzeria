d016e56ec4e1fcf783ec9360f6113001d0f5fcbe
Danciak <fatality1922@gmail.com>
2021-03-09 01:22:20 +0100

    test

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,43 @@ import Cart from './components/Cart.js';
 
 
 const app = {
+
+  initPages: function () {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    thisApp.activatePage(thisApp.pages[0].id);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        //get page id from href attribute
+        const id = clickedElement.getAttribute('href');
+        //run thisApp.activatePage with that id
+
+      });
+    }
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    //add class active to matching pages, remove from non matching
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    //add class active to matching link, remove from non matching
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
   initMenu: function () {
     const thisApp = this;
 
@@ -45,6 +82,8 @@ const app = {
     console.log('classNames:', classNames);
     console.log('settings:', settings);
     console.log('templates:', templates);
+
+    thisApp.initPages();
 
     thisApp.initData();
     thisApp.initCart();
